ce31179c64cdb94bd2df7c02a2c06c370d6a095d
deniscovei <58047883+deniscovei@users.noreply.github.com>
2023-04-08 17:21:04 +0300

    Update main.c

--- main.c
+++ main.c
@@ -23,20 +23,25 @@ int main(void)
 			arg_count = scanf("%ld", &size);
 			CHECK_ARGS(true, arg_count, 1, arena = alloc_arena(size));
 			break;
+
 		case ALLOC_BLOCK:
 			arg_count = scanf("%ld%ld", &address, &size);
 			CHECK_ARGS(true, arg_count, 2, alloc_block(arena, address, size));
 			break;
+
 		case DEALLOC_ARENA:
 			CHECK_ARGS(true, arg_count, 0, dealloc_arena(arena));
 			return 0;
+
 		case FREE_BLOCK:
 			arg_count = scanf("%ld", &address);
 			CHECK_ARGS(true, arg_count, 1, free_block(arena, address));
 			break;
+
 		case PMAP:
 			CHECK_ARGS(true, arg_count, 0, pmap(arena));
 			break;
+
 		case WRITE:
 			arg_count = scanf("%ld%ld", &address, &size);
 			fgetc(stdin);
@@ -44,16 +49,19 @@ int main(void)
 			ignore_buffer(data, size);
 			CHECK_ARGS(false, arg_count, 2, write(arena, address, size, data));
 			break;
+
 		case READ:
 			arg_count = scanf("%ld%ld", &address, &size);
 			CHECK_ARGS(true, arg_count, 2, read(arena, address, size));
 			break;
+
 		case MPROTECT:
 			arg_count = scanf("%ld", &address);
 			fgets(perm_keys_merged, BUFF_SIZE, stdin);
 			perm = get_mask(perm_keys_merged);
 			CHECK_ARGS(false, arg_count, 1, mprotect(arena, address, perm));
 			break;
+
 		default:
 			DISPLAY_ERROR(INVALID_COMMAND);
 		}
